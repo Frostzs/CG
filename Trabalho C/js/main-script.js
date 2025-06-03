ee999596891f94012c406e43386ff4bc0e14a374
Eduardo Barata <eduardo.castilho.barata@tecnico.ulisboa.pt>
2025-06-03 16:42:28 +0100

    code structure implemented

--- Trabalho C/js/main-script.js
+++ Trabalho C/js/main-script.js
@@ -8,8 +8,38 @@ let canvasSize = 512;
 let groundPlane, skyDome;
 let generateGroundTexture = false;
 let generateSkyTexture = false;
-let scene, camera, renderer;
+let scene, activeCamera, renderer;
 
+var cameras = []
+
+var moon, ovni, house, tree;
+// var skydome, terrain;
+
+var treePos = [], trees = [];
+
+/*
+// Lights
+var ambientLight = new THREE.AmbientLight( cor, intensidade? );
+var globalLight = new THREE.DirectionalLight( cor, intensidade? );
+var whatMAterial = "???";
+*/
+
+// Materials
+const materials = new Map(), materialsLAmbert = new Map(), materialsPhong = new Map(), 
+materialsToon = new Map(), materialBAsic = new Map(), clock = new THREE.Clock();
+
+const keys = {}, movementVector = new THREE.Vector2(0, 0);
+
+// Shade
+let shadeCalculation = true;
+
+// colors
+const yellow = 0xf2b632;
+const white = 0xffffff;
+const brown = 0x5a3825;
+const tileOrange = 0xb55239;
+const blue = 0x003366;
+const black = 0xffffff;
 
 
 function generateFloralTexture() {
@@ -111,9 +141,15 @@ function createScene() {
 //////////////////////
 function createCamera() {
     const aspect = window.innerWidth / window.innerHeight;
-    camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
-    camera.position.set(0, 10, 50);
-    camera.lookAt(0, 0, 0);
+    const pers = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+    pers.position.set(0, 10, 50);
+    pers.lookAt(0, 0, 0);
+
+    cameras = {
+        perspective: pers
+    };
+
+    activeCamera = cameras.perspective;
 }
 
 
@@ -121,10 +157,142 @@ function createCamera() {
 /////////////////////
 /* CREATE LIGHT(S) */
 /////////////////////
+function updateLights() {
+    'use strict';
+    // TO DO: Update lights if needed
+}
+
+///////////////////////
+/* SHADE CALCULATION */
+///////////////////////
+function updateShadeCalculation() {
+    'use strict';
+    // TO DO: Update shade calculation if needed
+}
 
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+function createFlowers() {
+    'use strict';
+    // TO DO: Create flowers as Object3D(IF needed)
+}
+
+function createStars() {
+    'use strict';
+    // TO DO: Create stars as Object3D
+}
+
+function createDegrade() {
+    'use strict';
+    // TO DO: Create degrade as Object3D(????)
+}
+
+function createSkydome() {
+    'use strict';
+    // TO DO: Create skydome as Object3D
+}
+
+function createTerrain() {
+    'use strict';
+    // TO DO: Create terrain as Object3D
+}
+
+function createTree() {
+    'use strict';
+    // TO DO Rodrigo: Create tree as Object3D
+
+    // Tree trunk
+
+    // Branches
+
+    // Leaves
+}
+
+function createMoon() {
+    'use strict';
+    // TO DO: Create moon as Object3D
+}
+
+function createOvniLights() {
+    'use strict';
+    // TO DO: Create ovni lights as Object3D
+}
+
+function createOvni(x, y, z) {
+    'use strict';
+    // TO DO: Create ovni as Object3D
+}
+
+function createbaseTrim() {
+    'use strict';
+    // TO DO Rodrigo: Create base trim as Object3D
+}
+
+function createWalls(x, y, z){
+    'use strict';
+    // TO DO Rodrigo: Create walls as Object3D
+
+    // Side Wall
+
+    // Front Wall
+
+    // Back Wall
+
+}
+
+function createframeWindows() {
+    'use strict';
+    // TO DO Rodrigo: Create frame windows as Object3D
+
+    // Sides Window Frame
+
+    // Top Window Frame
+
+    // Bottom Window Frame
+}
+
+function createWindows(x, y, z) {
+    'use strict';
+    // TO DO Rodrigo: Create windows as Object3D
+
+    // Side Window
+
+    // Front Windows
+
+    // Door Window
+}
+
+function createDoor(x, y, z) {
+    'use strict';
+    // TO DO Rodrigo: Create door as Object3D
+    
+    // Door
+
+    // Door Window?
+
+}
+
+function createCeiling(){
+    'use strict';
+    // TO DO Rodrigo: Create ceiling as Object3D
+}
+
+function createAlentejoHouse(x, y, z) {
+    'use strict';
+    // TO DO Rodrigo: Create Alentejo house as Object3D
+    houseAlentejo = new THREE.Object3D();
+
+    createWalls(x, y, z);
+
+    createWindows(x, y, z);
+
+    createDoor(x, y, z);
+
+    createCeiling();
+
+    scene.add(houseAlentejo);
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -170,13 +338,15 @@ function init() {
 /* ANIMATION CYCLE */
 /////////////////////
 function render() {
-    renderer.render(scene, camera);
+    renderer.render(scene, activeCamera);
 }
 
 function animate() {
-    requestAnimationFrame(animate);
     update();
+    
     render();
+    
+    requestAnimationFrame(animate);
 }
 
 
@@ -184,9 +354,12 @@ function animate() {
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
 function onResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        activeCamera.aspect = window.innerWidth / window.innerHeight;
+        activeCamera.updateProjectionMatrix();
+    }
 }
 
 
